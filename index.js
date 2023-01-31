9f9a947468dd37d45765c8590a2f197686c7b0cb
Besufekad Alemu <besurye@gmail.com>
2023-01-31 10:43:34 +0300

    Use camelCasing convention in my varaible name declaration, add descriptive name for event listener object

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // JS code goes here
 const hamburger = document.querySelector('.hamburger')
-const mobnav = document.querySelector('.mob__nav__menu')
-const nvsection = document.querySelector('.nv__section')
+const mobNav = document.querySelector('.mob__nav__menu')
+const nvSection = document.querySelector('.nv__section')
 const hamburger2 = document.querySelector('.mob__nav__menu .hamburger2')
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active')
-  mobnav.classList.toggle('active')
-  nvsection.classList.toggle('active')
+  mobNav.classList.toggle('active')
+  nvSection.classList.toggle('active')
 })
 
-document.querySelectorAll('.nv__item').forEach((n) =>
-  n.addEventListener('click', () => {
+document.querySelectorAll('.nv__item').forEach((item) =>
+  item.addEventListener('click', () => {
     hamburger.classList.remove('active')
     hamburger.style.display = 'block'
-    mobnav.classList.toggle('active')
+    mobNav.classList.toggle('active')
   })
 )
 
 hamburger2.addEventListener('click', () => {
   hamburger.style.display = 'block'
   hamburger.classList.remove('active')
-  mobnav.classList.toggle('active')
+  mobNav.classList.toggle('active')
 })
